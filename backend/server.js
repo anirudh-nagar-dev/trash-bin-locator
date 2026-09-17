83b01fa3889452956bb47e2e6428fec37db0ffdd
Anirudh Nagar <acaptainvortex@gmail.com>
2026-09-17 23:50:42 +0530

    Allow production Vercel domain in CORS

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cors({
   origin:[
     'http://localhost:5173',
-    'https://trash-bin-locator.vercel.app'
+    'https://trash-bin-locator-git-main-anirudh-nagar-dev.vercel.app'
   ]}));
 
 app.get("/", (req,res) => {
